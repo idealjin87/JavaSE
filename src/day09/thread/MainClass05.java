dcb1cfbcd914069a3c51ad697b6168dbffed53e3
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-04-05 22:01:26 +0900

    day09

---
+++ src/day09/thread/MainClass05.java
@@ -0,0 +1,23 @@
+package day09.thread;
+/*
+ * [[ Runnable ]]
+ *  Thread 상속대신 인터페이스 상속으로 Thread 객체 생성하기
+ * 
+ */
+
+import day09.mypac.InterThread;
+
+public class MainClass05 {
+	public static void main(String[] args) {
+		System.out.println("메인스레드 시작됩니다.");
+		
+		// 다형성 Runnable 인터페이스를 구현한 객체 생성
+		Runnable iThread = new InterThread();
+		//InterThread 생성한 객체를 Thread 객체 생성자 인자로 전달
+		Thread thread = new Thread(iThread);
+		// Thread 객체의 start() 메소드 호출하여 새로운 스레드 시작!
+		thread.start();
+		
+		System.out.println("메인스레드 종료됩니다.");
+	}
+}
