a15d7c0cc54ff063427ba2bde2982add39bb08d7
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-04-14 21:49:22 +0900

    day12

---
+++ src/day11/util/MainClass11.java
@@ -0,0 +1,38 @@
+package day11.util;
+
+import java.util.Calendar;
+
+/*
+ *  [[ Calendar Class ]]
+ *   날짜 관련 객체
+ *   날짜 계산용으로 자주사용
+ *   Date 객체와 호환 가능
+ * 
+ */
+public class MainClass11 {
+	public static void main(String[] args) {
+		// Calendar 객체 생성
+		// gerInstance 메소드를 통해서만 객체 생성 가능
+		Calendar cal = Calendar.getInstance();
+//		Calendar cal2 = Calendar.getInstance();
+//		
+//		System.out.println(System.identityHashCode(cal));
+//		System.out.println(System.identityHashCode(cal2));
+		
+		int year = cal.get(Calendar.YEAR);
+		System.out.println("올해 : " + year);
+		
+		int month = cal.get(Calendar.MONTH) + 1;
+		System.out.println("월 : " + month);
+		
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		System.out.println("일 : " + day);
+		
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int min = cal.get(Calendar.MINUTE);
+		int sec = cal.get(Calendar.SECOND);
+		
+		System.out.println(hour + ":" + min + ":" + sec);
+		
+	}
+}
